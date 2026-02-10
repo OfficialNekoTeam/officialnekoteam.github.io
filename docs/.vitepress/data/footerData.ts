74b807ae885a85b0c31d43ff9e4ab0733c573afc
初然Neko <2796331258@qq.com>
2026-02-11 04:25:34 +0800

    docs: 更新页脚链接
    - 替换 Vercel-App 为 Vercel-Docs 链接
    - 更新社区链接：新增 QQ、QQ频道、飞书等官方社群入口

--- docs/.vitepress/data/footerData.ts
+++ docs/.vitepress/data/footerData.ts
@@ -8,7 +8,7 @@ export const Footer_Data: FooterData = {
       icon: 'fa-solid fa-cloud',
       links: [
         { name: 'Vercel-NekoBot', link: 'https://docs.nekobot.dev/' },
-        { name: 'Vercel-App', link: 'https://nekobotteamgithubio.vercel.app/' },
+        { name: 'Vercel-Docs', link: 'https://nekobot-docs.vercel.app/' },
         { name: 'Github Page', link: 'https://nekobotteam.github.io/' },
       ],
     },
@@ -16,8 +16,11 @@ export const Footer_Data: FooterData = {
       title: '社区',
       icon: 'fa-solid fa-users',
       links: [
-        { name: '官方 企鹅1号社区', link: 'https://qm.qq.com/q/eOrRe4t9XW' },
-        { name: 'Neko猫猫 开发者群聊', link: 'https://qm.qq.com/q/G76NPmltSu' },
+        { name: 'Neko的猫猫窝', link: 'https://qm.qq.com/q/xW235wdw2c' },
+        { name: 'Neko猫猫的大学', link: 'https://qm.qq.com/q/cakIFXheeY' },
+        { name: 'Neko猫猫的小窝(QQ频道)', link: 'https://pd.qq.com/s/de131bcm9' },
+        { name: '开发者群聊(飞书)', link: 'https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=3d2gb6d5-1044-4376-9d84-d3c207007fce' },
+        { name: '社区意见提议(飞书)', link: 'https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=f76s0727-817c-4105-888c-69e00ee36a27' },
       ],
     },
   ],
