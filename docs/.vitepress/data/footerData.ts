17e010a4eff39c539edaf7191901b7f9df09e948
初然Neko <2796331258@qq.com>
2026-03-11 18:05:29 +0800

    docs: update public community links
    Remove the private QQ group from the docs footer and align the public community links with the organization profile README.

--- docs/.vitepress/data/footerData.ts
+++ docs/.vitepress/data/footerData.ts
@@ -16,8 +16,8 @@ export const Footer_Data: FooterData = {
       title: '社区',
       icon: 'fa-solid fa-users',
       links: [
-        { name: 'Neko的猫猫窝', link: 'https://qm.qq.com/q/xW235wdw2c' },
-        { name: 'Neko猫猫的大学', link: 'https://qm.qq.com/q/cakIFXheeY' },
+        { name: '玲可猫猫的大学喵(插件开发者群聊)', link: 'https://qm.qq.com/q/UJVig4ury8' },
+        { name: '玲可猫猫的大学 1群', link: 'https://qm.qq.com/q/cakIFXheeY' },
         { name: 'Neko猫猫的小窝(QQ频道)', link: 'https://pd.qq.com/s/de131bcm9' },
         { name: '开发者群聊(飞书)', link: 'https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=3d2gb6d5-1044-4376-9d84-d3c207007fce' },
         { name: '社区意见提议(飞书)', link: 'https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=f76s0727-817c-4105-888c-69e00ee36a27' },
